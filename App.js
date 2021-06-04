9fe1d7cd6bac345d3132cbcebd61abf813aec47a
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-06-04 09:03:41 -0300

    Post  component  created

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'
 import Header from './src/components/Header'
+import Post  from './src/components/Post'
 
 export default props => {
 	return (
-		<Header />
+		<View>
+			<Header />
+			<Post image={require('./assets/imgs/r1.png')} />
+		</View>
+		
 	)
 }
