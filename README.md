d229fd5033f3b55945d64688595898b6591cfbb0
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-06-04 08:41:50 -0300

    adjustments and Header component creation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # React Native InstaGreen
+
+That's an Instagram clone to review all the main tools and logics I've learned in React Native.
+
+This project is in development at this very moment (:
+
+🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+    StyleSheet,
+    View,
+    Text,
+    Platform,
+    Image,
+    SafeAreaView
+} from 'react-native'
+import logo from '../../assets/imgs/logo.png'
+
+export default props => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.rowContainer}>
+                <Image source={logo} style={styles.image} />
+                <Text style={styles.title}> InstaGreen </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 0,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#BBB'
+    },
+
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        margin: 5
+    },
+    image: {
+        height: 30,
+        width: 30,
+        resizeMode: 'contain'
+    },
+    title: {
+        color: '#000',
+        fontFamily: 'Futura',
+        fontSize: 28,
+        height: 30
+    }
+})
