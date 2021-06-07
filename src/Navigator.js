68a40f8ad80c98001a0f1f194564eddd40d56c5b
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-06-07 11:02:28 -0300

    navigation with bottom-tabs implemented

---
+++ src/Navigator.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Feed from './screens/Feed'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+Ionicons.loadFont()
+
+const Tab = createBottomTabNavigator()
+
+const MyTabs = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator 
+                tabBarOptions={{
+                    activeTintColor: 'green',
+                    inactiveTintColor: 'gray'
+                  }}
+                screenOptions={({route})=>({
+                    tabBarIcon:({ focused, tintColor })=>{
+                        let iconName
+
+                        if (route.name === 'Home') {
+                            iconName = focused ? 'md-home' : 'md-home-outline'
+
+                        } else if(route.name === 'Camera') {
+                            iconName = focused ? 'camera' : 'camera-outline'
+
+                        } else if(route.name === 'User'){
+                            iconName = focused ? 'md-person' : 'md-person-outline'
+                        } 
+                        return <Ionicons name={iconName} size={30} color='green' /> //<FontAwesome icon={iconName} tintColor={tintColor} />
+                    }})}>
+                <Tab.Screen name='Home' component={Feed} />
+                <Tab.Screen name='Camera' component={Feed} />
+                <Tab.Screen name='User' component={Feed} />
+            </Tab.Navigator>
+        </NavigationContainer>
+      )
+}
+
+export default MyTabs
