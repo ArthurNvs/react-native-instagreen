dbdc28d8e872fc2676902f8caad5e78f1f6af750
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-06-07 08:21:45 -0300

    colors adjustment

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -48,6 +48,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'white'
+        backgroundColor: '#011800'
     }
 })
