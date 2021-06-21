3e358327bb0a0719fdf92e85add170fc229e2995
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-06-21 16:27:38 -0300

    firebase functions implementation

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,7 +13,6 @@ import AddComment from './AddComment'
 
 const Post = props => {
     const addComment = props.name ? <AddComment postId={props.id} /> : null
-    console.warn(props.image)
     return (
             <View style={styles.container}>
                 <Image source={props.image} style={styles.image} />
